cd68fa6a86550f6ef2fd4165a8bb71bc6aaf9bfe
worldintro <worldintro@mail.ru>
2018-10-16 21:18:27 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+def square(x):
+  return x*x
+
+def mymax(a, b):
+    U = a if a > b else b
+    return U
+
+for N in range(1,4):
+  print(N, "this square", square(N))
+
+def scroll():
+    while 0 :
+        for T in range(1,5):
+            print("|")
